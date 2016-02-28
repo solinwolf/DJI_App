1413f3318eaaf72d87feea966c4f4e2bf50c5a94
root <zhangshenglin>
2016-02-28 13:10:19 +0800

    add guidance functions

--- src/guidance.cpp
+++ src/guidance.cpp
@@ -1,3 +1,7 @@
 #include "../include/guidance.h"
-
+float Read_Distance(int channal)
+{
+    float ret = -1;
+    return ret;
+}
 
---
+++ src/include/guidance.h
@@ -0,0 +1,7 @@
+#ifndef __GUIDANCE_H__
+#define __GUIDANCE_H__
+
+float Read_Distance(int channal);
+
+#endif 
+
